2d7a013e45aa113bd8f46f1900b9e14cadc456a0
Changhyun <meeso0219@gmail.com>
2024-03-13 11:41:02 +0900

    lab0 skeleton

---
+++ lab0.cpp
@@ -0,0 +1,17 @@
+// Changhun, Park
+// lab0.cpp
+// 03/13/24
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome message" << endl;
+    int arr[10];
+
+    cout << endl << "End of Lab 0 Program" << endl;
+
+    return 0;
+}
